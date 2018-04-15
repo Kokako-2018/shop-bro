f46f19cb5604e7d9f3e4b8f31bc7daddf1c66c0e
engie15 <e.matene15@gmail.com>
2018-04-16 07:56:43 +1200

    begun transfering fiiles to react-router format

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import Home from "./Home"
 import Item from './Item'
 import Cost from './Cost'
 
@@ -37,51 +39,9 @@ class App extends React.Component {
 
     render() {
         return <div>
-            <section class="hero is-medium" id="hero-header">
-                {/* <!-- Hero head: will stick at the top --> */}
-                <div class="hero-head">
-                    <nav class="navbar" id="navbar-bottom">
-                     <div class="container">
-                        <div class="navbar-brand">
-                            <div class="container">
-                                <h1 class="title is-2" id="title-font">
-                                    Shop-Bro
-                                </h1>
-                            </div>
-                            <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
-                        </div>
-                        <div id="navbarMenuHeroA" class="navbar-menu">
-                        <div class="navbar-end">
-                            <a class="navbar-item is-active">
-                              Settings
-                            </a>
-                            <a class="navbar-item">
-                              Savings
-                            </a>
-                            <a class="navbar-item">
-                              Feedback
-                            </a>
-                        </div>
-                        </div>
-                     </div>
-                    </nav>
-                </div>
-                {/* <!-- Hero content: will be in the middle --> */}
-                {/* <hr /> */}
-                <div class="hero-body">
-                    <div class="container has-text-centered">
-                    <h2 class="subtitle" id="subtitle">
-                    Making sure you stick to your shopping budget so you can smash your savings goals!
-                    </h2>
-                    </div>
-                </div>
-            </section>
-            
-            
+        <Router> 
+            <div>  
+            <Route exact path = '/' component = {Home} />
             <div class="column">
                 <div class="field has-addons">
                     <div class="control is-centered">
@@ -109,6 +69,8 @@ class App extends React.Component {
                 <Cost cost={this.cost} />
               </div>
             </div>
+            </div>
+        </Router>
         </div>
     }
 }
---
+++ client/components/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+const Home = () => {
+    return (
+      <div>
+        <section class="hero is-medium" id="hero-header">
+            {/* <!-- Hero head: will stick at the top --> */}
+            <div class="hero-head">
+                <nav class="navbar" id="navbar-bottom">
+                    <div class="container">
+                    <div class="navbar-brand">
+                        <div class="container">
+                            <h1 class="title is-2" id="title-font">
+                                Shop-Bro
+                            </h1>
+                        </div>
+                        <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+                    <div id="navbarMenuHeroA" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item is-active">
+                            Settings
+                        </a>
+                        <a class="navbar-item">
+                            Savings
+                        </a>
+                        <a class="navbar-item">
+                            Feedback
+                        </a>
+                    </div>
+                    </div>
+                    </div>
+                </nav>
+            </div>
+            {/* <!-- Hero content: will be in the middle --> */}
+            {/* <hr /> */}
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                <h2 class="subtitle" id="subtitle">
+                Making sure you stick to your shopping budget so you can smash your savings goals!
+                </h2>
+                </div>
+            </div>
+        </section>
+        <Link to = {'/savings'} />
+      </div>
+    )
+}
