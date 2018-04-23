68b405695debf6e1a44303be7f5a73ac6496acf9
engie15 <e.matene15@gmail.com>
2018-04-23 21:19:36 +1200

    creating a branch to fix get-item-to-update functionality

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function ListItem ({item}) {
-    // console.log(item.item, 'here')
+    console.log(item, 'here')
     return (
         <div className='list-item'>
          <div class="columns">
@@ -10,6 +10,7 @@ export default function ListItem ({item}) {
             <li>Item: {`${item.item}`}</li>
             <li>Cost: {`${item.cost}`}</li>
           </ul>
+          <button id={`${item}`}>Update</button>
          </div>
          </div>
         </div>
